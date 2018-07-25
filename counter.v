b6a1feedd31398daefea9e262298eedbc1c085f9
ajithch <41702712+ajithch@users.noreply.github.com>
2018-07-26 00:49:30 +0530

    Add files via upload

---
+++ counter.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:55:03 11/17/2017 
+// Design Name: 
+// Module Name:    counter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module counter()
+reg[3:0] counter, counter_next;
+
+//sequential block
+always @(posedge clk) 
+   counter <= counter_next;
+
+//combinational block
+counter_next = counter + 4'b1;
---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "G:/verilog/verilog assign/testbench_isim_beh.exe" -prj "G:/verilog/verilog assign/testbench_beh.prj" "work.testbench" "work.glbl" 
